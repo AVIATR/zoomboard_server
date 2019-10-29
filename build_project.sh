59b624ae312da909cb5fffbeeedc3e170493ae40
Ender Tekin <etekin@wisc.edu>
2019-10-29 14:33:57 -0500

    Updated the build script

--- build_project.sh
+++ build_project.sh
@@ -1,72 +1,80 @@
 #!/bin/bash
 export PROJ_NAME="zoomboard_server"
 function usage {
-    echo "Usage: $0 [-x|--xcode] [-h|--help] [-b|--build] [build_dir]"
+    echo "Usage: $0 [-x] [-h] [-b config] [build_dir]"
     echo " -x               Create xcode project. By default, a make project is created."
     echo " -h               This help message."
-    echo " -b [config]      Builds the project binaries after the project is built. config is either 'Release' or 'Debug'"
+    echo " -b config        Builds the project binaries after the project is built; config is either 'Release' or 'Debug'"
     echo " build_dir        Name of folder to put the build files in. Default is 'build'."
 }
 
-#if [ -z "$1" ]; then
-#    usage
-#    exit 1
-#fi
-TARGET_DIR='build'
-CONFIG='Debug'
+function error {
+    usage
+    exit 1
+}
 
-#ARGS=(-DBUILD_SHARED_LIBS=YES)
-while [[ $# -gt 0 ]]
-do
-key="$1"
-case $key in
-    -h|--help)
-        usage
-        exit 0
-        ;;
-    -x|--xcode)
-        IS_XCODE=yes
-        ARGS+=(-G Xcode)
-        shift #past switch
-        ;;
-    -b|--build)
-        ARGS+=(--build)
-        DO_BUILD_BINARIES=yes
-        echo "Project binaries will be built."
-        shift #past switch
-        CONFIG="$1"
-        shift
-        ;;
-    *)    # unknown option
-        TARGET_DIR="$1" # save it in an array for later
-        shift # past argument
-        ;;
-esac
+while getopts ":hxb:" opt; do
+    case ${opt} in
+        h )
+            usage
+            exit 0
+            ;;
+        x )
+            ARGS+=(-G Xcode)
+            ;;
+        b )
+            CONFIG=$OPTARG
+            ;;
+        \? )
+            echo "Invalid option: $OPTARG" 1>&2
+            error
+            ;;
+        : )
+            echo "Invalid option: $OPTARG requires an argument" 1>&2
+            error
+            ;;
+    esac
 done
+shift $((OPTIND -1))
+
+if [ ${#@} -gt 1 ]; then
+    echo "Multiple arguments provided for build dir: $@" 1>&2
+    error
+elif [ -z "$@" ]; then
+    TARGET_DIR='build'
+else
+    TARGET_DIR="$@"    
+fi
 
 ARGS+=(-S $(pwd) -B "${TARGET_DIR}")
-echo CMAKE ARGS = "${ARGS[@]}"
 
+#Cleanup or create target folder
 if [[ -e "${TARGET_DIR}" ]]; then
     read -p "${TARGET_DIR} exists, and will be removed. Are you sure (y/n)? " CHOICE
-    echo    # (optional) move to a new line
+    echo    #move to a new line
     if [[ $CHOICE =~ ^[Yy]$ ]]; then
         rm -rf "${TARGET_DIR}"
     else
         echo "Exiting. Project has not been built."
-        exit
+        exit 0
     fi
 fi
 mkdir "${TARGET_DIR}"
 
-if [ "${IS_XCODE}" = yes ]; then
-    echo "Building Xcode project in ${TARGET_DIR}"
-else
-    echo "Building make project in ${TARGET_DIR}"
-fi
+#Build project
+echo "Configuring project..."
 cmake "${ARGS[@]}"
 
-if [ "${DO_BUILD_BINARIES}" = yes ]; then
-    echo "Building binaries"
-    cmake --build -B "${TARGET_DIR}" --config Release
+#Build binaries if requested
+if [ -n "$CONFIG" ]; then
+    case "$CONFIG" in
+        Release|Debug )
+            echo "Building binaries..."
+            cmake --build "$TARGET_DIR" --config $CONFIG -j4
+            ;;
+        * )
+            echo "Unknown build configuration: $CONFIG" 1>&2
+            error
+            ;; 
+    esac
 fi
